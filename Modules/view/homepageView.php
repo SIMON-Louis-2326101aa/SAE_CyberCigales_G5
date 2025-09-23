2741bbd25d2e64bf7e259c9c9c8a1588b09788e3
ImranGuzelbaba <imran.guzelbaba@etu.univ-amu.fr>
2025-09-23 11:00:05 +0200

    debut page

--- Modules/view/homepageView.php
+++ Modules/view/homepageView.php
@@ -1,1 +1,20 @@
 <?php
+// homepageView.php
+$message = 'Page d\'accueil';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+</head>
+<body>
+<h1><?php echo $message; ?></h1>
+
+<!-- Exemple de bouton qui envoie vers le contrôleur -->
+<form action="" method="post">
+    <button type="submit">Cliquer</button>
+</form>
+</body>
+</html>
