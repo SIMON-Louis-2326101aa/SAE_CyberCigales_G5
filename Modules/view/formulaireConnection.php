f9a14890e9db6a52ca3d8d97189230204b4978a9
Lou <lou.contrucci@etu.univ-amu.fr>
2025-09-27 15:29:31 +0200

    formulaire de connection non relier a la base de donnée

---
+++ Modules/view/formulaireConnection.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8"/>
+    <title> Connexion </title>
+</head>
+<h1> Connection </h1>
+<form action="formulaireConnection.php"  method="post">
+    Adresse mail <input type="email" id="mail"> <br>
+    Mot de passe <input type="password" id="mdp"><br>
+    <button type="submit">Connexion</button>
+
+    <!--Si vous n'avait pas de compte inscivez vous-->
+
+
+</form>
+</html>
