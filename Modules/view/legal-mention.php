c3b0181802ac6181ea7778f2e3678d1e7a18a393
ImranGuzelbaba <imran.guzelbaba@etu.univ-amu.fr>
2025-10-02 11:04:37 +0200

    Ajout du favicon dans la page mention légale

--- Modules/view/legal-mention.php
+++ Modules/view/legal-mention.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mentions Légales</title>
+    <link rel="icon" type="image/png" href="/public/assets/Image/favicon.png">
 </head>
 
 <body>
