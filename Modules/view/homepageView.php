e8ede45d0ab78dd3597a76e120f90417c5709166
Lou <lou.contrucci@etu.univ-amu.fr>
2025-10-02 09:36:01 +0200

    rajout boutton deconnection

--- Modules/view/homepageView.php
+++ Modules/view/homepageView.php
@@ -12,6 +12,7 @@ session_start();
 <body>
 <?php if (isset($_SESSION['user_id'])): ?>
     <h1>Bienvenue <?php echo htmlspecialchars($_SESSION['prenom'] . " " . $_SESSION['nom']); ?></h1>
+    <a href="/logout"><button>Deconnexion</button></a>
 <?php else: ?>
     <h1>Bienvenue</h1>
     <a href="/register"><button>Inscription</button></a>
