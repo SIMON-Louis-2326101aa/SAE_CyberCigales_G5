118c374dcafb154d2853a625b507b1eff41c0400
SIMON-Louis-2326101aa <louis.simon.3@etu.univ-amu.fr>
2025-10-07 20:13:52 +0200

    commit des reparation qui sont faites malgres la duplication

--- Modules/controller/formulaireConnectionController.php
+++ Modules/controller/formulaireConnectionController.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../controller/UserController.php';
 require __DIR__ . '/../view/formulaireConnection.php';
-class homePageController
+class formulaireConnectionController
 {
     public function login()
     {
--- Modules/controller/formulaireInscriptionController.php
+++ Modules/controller/formulaireInscriptionController.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../controller/UserController.php';
 require __DIR__ . '/../view/formulaireInsc.php';
-class homePageController
+class formulaireInscriptionController
 {
     public function login()
     {
