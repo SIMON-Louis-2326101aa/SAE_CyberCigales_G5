84c6a2b59f3d6a2350e407e4bd1c63c374fc7a6d
SIMON-Louis-2326101aa <louis.simon.3@etu.univ-amu.fr>
2025-10-08 21:59:25 +0200

    push de la supression d'un doublon return et des "mail" du formulaire inscription model

--- Modules/model/formInscriptionModel.php
+++ Modules/model/formInscriptionModel.php
@@ -7,25 +7,22 @@ class formInscriptionModel extends database
     {
         $sql = "INSERT INTO users (nom, prenom, email, password) VALUES (:nom, :prenom, :email, :password)";
         $stmt = $this->getBdd()->prepare($sql);
+        $hash = password_hash($password, PASSWORD_BCRYPT);
         return $stmt->execute([
             'nom' => $nom,
             'prenom' => $prenom,
             'email' => $email,
-            'password' => $password
+            'password' => $hash
         ]);
-
-        $hash = password_hash($password, PASSWORD_BCRYPT);
-        $stmt = $bdd->prepare("INSERT INTO users (nom, prenom, email, password)  VALUES (:nom, :prenom, :email, :password) ");
-        return $stmt->execute([ 'nom' => $nom, 'prenom' => $prenom, 'email' => $email, 'password' => $hash]);
     }
 
     public function findByEmail($email): bool
     {
         $bdd = $this->getBdd();
 
-        $sql = "SELECT COUNT(*) FROM users WHERE Mail = :mail";
+        $sql = "SELECT COUNT(*) FROM users WHERE email = :email";
         $stmt = $bdd->prepare($sql);
-        $stmt->bindParam(':mail', $mail, PDO::PARAM_STR);
+        $stmt->bindParam(':mail', $email, PDO::PARAM_STR);
         $stmt->execute(['email' => $email]);
         return $stmt->fetch() > 0;
     }
