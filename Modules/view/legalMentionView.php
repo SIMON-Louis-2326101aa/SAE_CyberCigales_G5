060b9242af2dc71d328c29503b3d317a20649422
Lou <lou.contrucci@etu.univ-amu.fr>
2025-10-09 23:09:26 +0200

    correction de texte

--- Modules/view/legalMentionView.php
+++ Modules/view/legalMentionView.php
@@ -16,9 +16,9 @@
     <div id="legal-mention">
         <h2 id="-mentions-l-gales-"><strong>Mentions Légales</strong></h2>
         <h3 id="-editeur-du-site-"><strong>Editeur du site</strong></h3>
-        <p>Le présent site web est édité par equipe 5 Cyber Cygales</p>
-        <p>Tél: 04 54 68 xx xx<br>Email : escapethecode2025@gmail.com <br>nom site : <a href="https://escapethecode.alwaysdata.net/">https://escapethecode.alwaysdata.net/</a></p>
-        <h3 id="-h-bergeur-"><strong>Hébergeur</strong></h3>
+        <p>Le présent site web est édité par BADJOUDJ Hana, CONTRUCCI Lou, DIAZ Gwenn, GUZELBABA Imran, SIMON Louis </p>
+        <p>Tél: 04 54 68 xx xx<br>Email : escapethecode2025@gmail.com <br>Nom du site : <a href="https://escapethecode.alwaysdata.net/">Escape The Code/</a></p>
+        <h3 id="host"><strong>Hébergeur</strong></h3>
         <p>Le présent site web est hébergé par :</p>
         <p>AlwaysData</p>
     </div>
