cb8d8dd222be401f40cc941f7dab989ac2f18ef2
Lou <lou.contrucci@etu.univ-amu.fr>
2025-10-09 23:09:44 +0200

    correction de texte

--- Modules/view/legalMentionView.php
+++ Modules/view/legalMentionView.php
@@ -17,7 +17,7 @@
         <h2 id="-mentions-l-gales-"><strong>Mentions Légales</strong></h2>
         <h3 id="-editeur-du-site-"><strong>Editeur du site</strong></h3>
         <p>Le présent site web est édité par BADJOUDJ Hana, CONTRUCCI Lou, DIAZ Gwenn, GUZELBABA Imran, SIMON Louis </p>
-        <p>Tél: 04 54 68 xx xx<br>Email : escapethecode2025@gmail.com <br>Nom du site : <a href="https://escapethecode.alwaysdata.net/">Escape The Code/</a></p>
+        <p>Tél: 04 54 68 xx xx<br>Email : escapethecode2025@gmail.com <br>Nom du site : <a href="https://escapethecode.alwaysdata.net/">Escape The Code</a></p>
         <h3 id="host"><strong>Hébergeur</strong></h3>
         <p>Le présent site web est hébergé par :</p>
         <p>AlwaysData</p>
