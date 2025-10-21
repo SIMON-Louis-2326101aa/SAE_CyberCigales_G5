7152d8319b17af8c2012e05eb2bd13f2400f8830
ImranGuzelbaba <imran.guzelbaba@etu.univ-amu.fr>
2025-10-21 11:18:10 +0200

    Ajout du bouton changer de theme et la fonctionnalité

--- includes/layout/header.php
+++ includes/layout/header.php
@@ -5,8 +5,10 @@
     <title><?php echo $A_params['pageTitle'] ?? 'CyberCigales'; ?></title>
     <link rel="stylesheet" href="/public/assets/styles/stylesheet.css">
     <link rel="icon" href="/public/assets/images/favicon.ico">
+    <script src="/public/assets/js/JSVide.js"></script>
 </head>
 <body>
 <nav>
     <a href="index.php?controller=homepage&action=openHomepage" class="active">Home</a>
+    <button class="theme-button"> Changer de Theme </button>
 </nav>
