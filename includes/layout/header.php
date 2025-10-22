3dc4cb0d8394df32e48ffb5af86eb2d88636d7c9
ImranGuzelbaba <imran.guzelbaba@etu.univ-amu.fr>
2025-10-22 16:56:31 +0200

    Avant merge au branche css
    # Conflicts:
    #	includes/layout/header.php

--- includes/layout/header.php
+++ includes/layout/header.php
@@ -5,15 +5,17 @@
     <title><?php echo $A_params['pageTitle'] ?? 'CyberCigales'; ?></title>
     <link rel="stylesheet" href="./assets/styles/stylesheet.css">
     <link rel="icon" href="./assets/images/favicon.ico">
+    <script src="/assets/js/JSVide.js"></script>
 </head>
 <body>
 <nav class="nav-menu">
 
     <div id="nav-menu-sections">
-        <a href="index.php?controller=redirection&action=openHomepage" class="active">Home</a>
+        <a href="index.php?controller=redirection&action=openHomepage" class="active theme-button">Home</a>
     </div>
 
     <div id="nav-auth-buttons">
+        <button class="theme-button">Changer de Theme</button>
         <a href="index.php?controller=user&action=register" class="active"><button>Inscription</button></a>
         <a href="index.php?controller=user&action=login" class="active"><button>Connexion</button></a>
     </div>
---
+++ public/assets/js/JSVide.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeButton = document.querySelector('.theme-button');
+    const body = document.body;
+
+    // Chargement du theme depuis localStorage
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        body.classList.add(currentTheme);
+    }
+
+    themeButton.addEventListener('click', () => {
+        body.classList.toggle('dark-theme');
+        
+        // Sauvegarde du theme dans localStorage
+        if (body.classList.contains('dark-theme')) {
+            localStorage.setItem('theme', 'dark-theme');
+        } else {
+            localStorage.removeItem('theme');
+        }
+    });
+});
