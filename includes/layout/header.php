b0364fa7025090bca402584fed4b4678061b42e9
Lou <lou.contrucci@etu.univ-amu.fr>
2025-10-22 14:16:50 +0200

    correction de lien

--- includes/layout/header.php
+++ includes/layout/header.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo $A_params['pageTitle'] ?? 'CyberCigales'; ?></title>
-    <link rel="stylesheet" href="/assets/styles/stylesheet.css">
-    <link rel="icon" href="/assets/images/favicon.ico">
+    <link rel="stylesheet" href="./assets/styles/stylesheet.css">
+    <link rel="icon" href="./assets/images/favicon.ico">
 </head>
 <body>
 <nav class="nav-menu">
 
     <div id="nav-menu-sections">
-        <a href="index.php?controller=homepage&action=openHomepage" class="active">Home</a>
+        <a href="index.php?controller=redirection&action=openHomepage" class="active">Home</a>
     </div>
 
     <div id="nav-auth-buttons">
