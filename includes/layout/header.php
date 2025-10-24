dcf2dfbd1f30c15c9f57221868fc0a8d3d8d83e7
GwennDiaz <gwenn.diaz@etu.univ-mu.fr>
2025-10-24 13:47:15 +0200

    changement de place du bouton accueil

--- includes/layout/header.php
+++ includes/layout/header.php
@@ -16,7 +16,6 @@ if (session_status() === PHP_SESSION_NONE) {
 
     <div id="nav-menu-sections">
         <a href="index.php?controller=redirection&action=openHomepage" id="nav-menu-title"><img src="/assets/images/logo.png" alt="logo-escape-the-code"></a>
-        <a href="index.php?controller=redirection&action=openHomepage" class="active btn-nav">Accueil</a>
     </div>
 
     <div id="nav-auth-buttons">
@@ -25,6 +24,7 @@ if (session_status() === PHP_SESSION_NONE) {
             ?>
             <!-- Changer de Theme marche comme un bouton mais l'ajout de javascript:void(0) permet de ne pas recharger la page -->
             <a href="javascript:void(0)" id="theme-changer" class="btn-nav">Changer de Theme</a>
+            <a href="index.php?controller=redirection&action=openHomepage" class="active btn-nav">Accueil</a>
             <a href="index.php?controller=user&action=logout" class="active btn-nav">Déconnexion</a>
             <a href="index.php?controller=user&action=account" class="active btn-nav">Compte</a>
             <?php
@@ -32,6 +32,7 @@ if (session_status() === PHP_SESSION_NONE) {
             ?>
             <!-- Changer de Theme d(0) -->
             <a href="javascript:void(0)" id="theme-changer" class="btn-nav">Changer de Theme</a>
+            <a href="index.php?controller=redirection&action=openHomepage" class="active btn-nav">Accueil</a>
             <a href="index.php?controller=user&action=register" class="active btn-nav">Inscription</a>
             <a href="index.php?controller=user&action=login" class="active btn-nav">Connexion</a>
             <?php
