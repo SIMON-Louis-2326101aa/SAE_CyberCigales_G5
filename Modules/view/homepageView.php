7c9c73defb8771eb51a28a30d0ac729ba3748215
GwennDiaz <gwenn.diaz@etu.univ-mu.fr>
2025-10-24 14:17:59 +0200

    rajout d'une image

--- Modules/view/homepageView.php
+++ Modules/view/homepageView.php
@@ -1,7 +1,19 @@
 <?php if (isset($_SESSION['user_id'])): ?>
-    <h1>Bonjour <?php echo htmlspecialchars($_SESSION['prenom'] . " " . $_SESSION['nom']); ?> !</h1>
-    <p>Vous êtes connecté(e) avec l'email : <?php echo htmlspecialchars($_SESSION['email']); ?></p>
+    <div class="hero-container">
+        <img src="/assets/images/img.png" alt="Arrière-plan de la page d'accueil" loading="lazy">
+
+        <h1>Bonjour <?php echo htmlspecialchars($_SESSION['prenom'] . " " . $_SESSION['nom']); ?> !</h1>
+        <p>Vous êtes connecté(e) avec l'email : <?php echo htmlspecialchars($_SESSION['email']); ?></p>
+    </div>
 
 <?php else: ?>
-    <h1>Bienvenue</h1>
+    <div class="hero-container">
+        <img src="/assets/images/img.png" alt="Arrière-plan de la page d'accueil" loading="lazy">
+
+        <h1>Bienvenue</h1>
+
+        <div>
+
+        </div>
+    </div>
 <?php endif; ?>
