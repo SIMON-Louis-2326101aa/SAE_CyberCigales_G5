255ac751b5f603fe316111888194db1b8197cae3
Lou <lou.contrucci@etu.univ-amu.fr>
2026-01-06 14:36:27 +0100

    ajustement du plan du site pour quand on est admin on peut voir toute les epreuves

--- Modules/view/siteMapView.php
+++ Modules/view/siteMapView.php
@@ -2,7 +2,28 @@
     <h1>Plan du site</h1>
     <ul class="sitemap-list">
         <?php
-        if (isset($_SESSION['utilisateur'])) {
+        if (isset($_SESSION['user_id']) && $_SESSION['user_id'] == 5) {
+            ?>
+            <li>
+                <a href="index.php?controller=Redirection&action=openHomepage">Accueil</a>
+                <a href="index.php?controller=Admin&action=listUser">Tableau de bord Admin</a>
+                <a href="index.php?controller=User&action=logout">Déconnexion</a>
+                <a href="index.php?controller=User&action=account">Compte</a>
+                <a href="index.php?controller=Redirection&action=openLegal">Mentions Légales</a>
+                <a href="index.php?controller=Redirection&action=openAbout">À propos</a>
+                <br>
+                <a href="index.php?controller=Redirection&action=openLetterIntro">Enigme 1</a>
+                <a href="index.php?controller=Redirection&action=openPicturePuzzle">Enigme 2</a>
+                <a href="index.php?controller=Redirection&action=openButterflyWay">Enigme 3</a>
+                <a href="index.php?controller=Redirection&action=openPhishingPuzzle">Enigme 4</a>
+                <a href="index.php?controller=Redirection&action=openPasswordGame">Enigme 5</a>
+                <a href="index.php?controller=Redirection&action=openSummaryClue">Enigme 6</a>
+                <a href="index.php?controller=Redirection&action=openSearchSM">Enigme 7</a>
+                <a href="index.php?controller=Redirection&action=openMeetingPwd">Enigme 8</a>
+                <a href="index.php?controller=Redirection&action=openEndText">Enigme Finale</a>
+            </li>
+            <?php
+        } elseif (isset($_SESSION['utilisateur'])) {
             ?>
             <li>
                 <a href="index.php?controller=Redirection&action=openHomepage">Accueil</a>
