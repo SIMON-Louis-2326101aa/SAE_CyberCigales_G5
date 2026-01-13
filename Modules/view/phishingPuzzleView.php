e4e017d87f7604376e6aae4587867e5a9f0f9f8e
SIMON-Louis-2326101aa <louis.simon.3@etu.univ-amu.fr>
2026-01-13 17:44:40 +0100

    Petite passerelle pour continuer les épreuves en attendant le phishing

--- Modules/view/phishingPuzzleView.php
+++ Modules/view/phishingPuzzleView.php
@@ -2,4 +2,5 @@
 
 ?>
 <h2>enigme phishing</h2>
-<p></p>
+<p>Cette page est en cours de développement...</p>
+<a href="index.php?controller=Redirection&action=openPasswordGame">Cliquez ici pour continuer !</a>
