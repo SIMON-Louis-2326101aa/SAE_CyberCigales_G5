19eecbca6151b360a8f815b4ab5e347403798623
Lou <lou.contrucci@etu.univ-amu.fr>
2026-02-11 13:41:02 +0100

    epreuve 10 finin et la photo

--- Modules/view/endTextView.php
+++ Modules/view/endTextView.php
@@ -1,11 +1,14 @@
 <?php
 
 ?>
-<p>Nous y sommes presque ils nous reste plus qu'a comprendre où se trouve le trésor !</p>
-<p>“Vous retrouverez nos souvenir la ou nous fêtions la vie, vous le trouverez sur le premier papier de cette vie,
+<div class="end">
+    <p>Nous y sommes presque ils nous reste plus qu'a comprendre où se trouve le trésor !</p>
+    <p>Il nous est dit : </p>
+    <p>“Vous retrouverez nos souvenir la ou nous fêtions la vie, vous le trouverez sur le premier papier de cette vie,
     la clef est dans ce jour si spécial ”</p>
 
 <img src="./assets/images/maps.png" alt="indice pour trouver le coffre" class="end-image">
+</div>
 
 <form action="index.php?controller=Puzzle&action=validateEnd" method="post" id="end-form">
     <label>
