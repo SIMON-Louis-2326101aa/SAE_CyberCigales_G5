dfb23be4b275349ee7b1b43059e273998def4cec
Lou <lou.contrucci@etu.univ-amu.fr>
2026-03-11 14:52:40 +0100

    ajout d'information sur le morse

--- Modules/view/infos/encryption.php
+++ Modules/view/infos/encryption.php
@@ -28,3 +28,69 @@
     </ul>
     <p>mais pour cela, il faut connaître la clé utilisée.</p>";
 </section>
+<section class="morse-info">
+
+    <h4>Le code Morse</h4>
+
+    <p>
+        Le code Morse est un système de communication inventé au XIXe siècle.
+        Il permet de transmettre des messages à l’aide de signaux courts et longs.
+        À l’origine, il était utilisé avec le télégraphe.
+    </p>
+
+    <p>
+        Chaque lettre est représentée par une combinaison de :
+    </p>
+
+    <ul>
+        <li>• un point (signal court)</li>
+        <li>– un tiret (signal long)</li>
+    </ul>
+
+    <h3>Alphabet Morse</h3>
+
+    <ul>
+        <li>A : • –</li>
+        <li>B : – • • •</li>
+        <li>C : – • – •</li>
+        <li>D : – • •</li>
+        <li>E : •</li>
+        <li>F : • • – •</li>
+        <li>G : – – •</li>
+        <li>H : • • • •</li>
+        <li>I : • •</li>
+        <li>J : • – – –</li>
+        <li>K : – • –</li>
+        <li>L : • – • •</li>
+        <li>M : – –</li>
+        <li>N : – •</li>
+        <li>O : – – –</li>
+        <li>P : • – – •</li>
+        <li>Q : – – • –</li>
+        <li>R : • – •</li>
+        <li>S : • • •</li>
+        <li>T : –</li>
+        <li>U : • • –</li>
+        <li>V : • • • –</li>
+        <li>W : • – –</li>
+        <li>X : – • • –</li>
+        <li>Y : – • – –</li>
+        <li>Z : – – • •</li>
+    </ul>
+
+    <h4>Exemple</h4>
+
+    <p>
+        Le célèbre signal de détresse <em>SOS</em> en Morse est :
+    </p>
+
+    <p>
+        • • •   – – –   • • •
+    </p>
+
+    <p>
+        Astuce : dans un message Morse, les lettres sont séparées par un petit espace
+        et les mots par un espace plus long.
+    </p>
+
+</section>
